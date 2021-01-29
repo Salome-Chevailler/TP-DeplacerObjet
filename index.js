97218139a848c069b2d68d05992fb38557dc45f2
Salome-Chevailler <74154174+Salome-Chevailler@users.noreply.github.com>
2021-01-29 10:15:33 +0100

    Début question a)

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("bonjour");
+// a) Déplacer la carte en utilisant les touches de direction du clavier
+
+document.addEventListener("keydown", saisie);
+
+function saisie(evt) {
+  console.log(evt.key);
+}
